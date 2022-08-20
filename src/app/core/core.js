23b687e6ee87e87930327e39c4459efd62c2db70
Brendson Victor <brendson.dev@gmail.com>
2022-08-20 13:56:37 -0300

    feat: iniciando rota de update

--- src/app/core/core.js
+++ src/app/core/core.js
@@ -13,3 +13,13 @@ exports.home = (req, res) => {
 exports.add_user = (req, res) => {
   res.render('add_user')
 }
+
+exports.update_user = (req, res) => {
+  axios.get('http://localhost:3000/api/users', { params: { id: req.query.id } })
+    .then(function (userdata) {
+      res.render('update_user', { user: userdata.data })
+    })
+    .catch(err => {
+      res.send(err)
+    })
+}
